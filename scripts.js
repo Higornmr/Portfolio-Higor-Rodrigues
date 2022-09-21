a27c85e7485c4e918a417c24d546d1703e92f5a4
Higor Rodrigues <higor_1992@hotmail.com>
2022-09-21 13:06:59 -0300

    Melhorias no portfolio

--- scripts.js
+++ scripts.js
@@ -1,19 +1,58 @@
+
+const changeThemeBtn = document.querySelector("#change-theme");
 const btn = document.getElementById("btnTop");
 
-btn.addEventListener("click", function () {
-    window.scrollTo(0, 0);
+
+function toggleDarkMode() {
+  document.body.classList.toggle("dark");
+}
+
+function loadTheme() {
+  const darkMode = localStorage.getItem("dark");
+
+  if (darkMode) {
+    toggleDarkMode();
+  }
+}
+
+loadTheme();
+
+changeThemeBtn.addEventListener("change", function () {
+  toggleDarkMode();
+
+  localStorage.removeItem("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("dark", 1);
+  }
 });
 
-window.onscroll = function() {scrollFunction()};
+btn.addEventListener("click", function () {
+  window.scrollTo(0, 0);
+});
+
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        btn.style.display = "block";
-    } else {
-        btn.style.display = "none";
-    }
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
   }
+}
 
-
-
-
+const observer = new IntersectionObserver(entries => {
+    Array.from(entries).forEach(entry =>{
+        if (entry.intersectionRatio >= 1){
+            entry.target.classList.add('init-hidden-off')
+        }
+    })
+    
+}, {
+    threshold: [0, .5, 1]
+})
+Array.from(document.querySelectorAll('.init-hidden')).forEach(element => {
+    observer.observe(element)
+})
